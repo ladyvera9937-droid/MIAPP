4376ff020d4019eb2bf8fd043b6be4496bb921c4
Lady <ladyvera9937@gmail.com>
2025-11-28 18:59:47 -0500

    Clase 28

--- MIAPP/src/app/paginas/componentes/header-global/header-global.component.ts
+++ MIAPP/src/app/paginas/componentes/header-global/header-global.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone'
+import {IonContent, IonHeader, IonTitle, IonToolbar, IonButtons} from '@ionic/angular/standalone'
 @Component({
   selector: 'app-header-global',
   templateUrl: './header-global.component.html',
   styleUrls: ['./header-global.component.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButtons ]
 })
 export class HeaderGlobalComponent  implements OnInit {
 
--- MIAPP/src/app/paginas/vermas/vermas.page.ts
+++ MIAPP/src/app/paginas/vermas/vermas.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
+import { HeaderGlobalComponent } from '../componentes/header-global/header-global.component';
 
 @Component({
   selector: 'app-vermas',
   templateUrl: './vermas.page.html',
   styleUrls: ['./vermas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderGlobalComponent]
 })
 export class VermasPage implements OnInit {
 
